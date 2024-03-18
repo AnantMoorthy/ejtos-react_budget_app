0ddee4b688fd5f6b95a6146a0249ede51ff065e4
AnantMoorthy <moorthyanant@gmail.com>
2024-03-18 08:52:24 -0400

    changes made from theia lab environment

--- src/components/CurrencyList.js
+++ src/components/CurrencyList.js
@@ -14,12 +14,12 @@ const CurrencyList = () => {
     
 
   return (
-        <div className='alert alert-secondary'style={{color:"white", backgroundColor:"lightgreen"}}> Currency: {
+        <div className='alert alert-secondary'style={{color:"white", backgroundColor:"lightgreen", content:"()"}}> Currency: {
       <select name="currency" id="currency" style={{ color:"white", backgroundColor: "lightgreen", border: '1px solid lightgreen'}} onChange={event=>changeCurrency(event.target.value)}>
-        <option value="£"style={{color:"black", backgroundColor:"lightgreen"}}>Pound(£)</option>
-        <option value="₹"style={{color:"black", backgroundColor:"lightgreen"}}>Rupee(₹)</option>
-        <option value="€"style={{color:"black", backgroundColor:"lightgreen"}}>Euro(€)</option>
-        <option value="$"style={{color:"black", backgroundColor:"lightgreen"}}>Dollar($)</option>
+        <option value="$"style={{color:"black", backgroundColor:"lightgreen"}}>$ Dollar</option>
+        <option value="£"style={{color:"black", backgroundColor:"lightgreen"}}>£ Pound</option>
+        <option value="€"style={{color:"black", backgroundColor:"lightgreen"}}>€ Euro</option>
+        <option value="₹"style={{color:"black", backgroundColor:"lightgreen"}}>₹ Rupee</option>
       </select>	
       }	
     </div>
